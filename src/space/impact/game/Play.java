ce9bbfe259d8458f5fd6647df0f7ef48ec48c388
sirMasterEgg <victor_s20@mhs.istts.ac.id>
2021-05-04 21:22:23 +0700

    commit login awal finish

--- src/space/impact/game/Play.java
+++ src/space/impact/game/Play.java
@@ -1,5 +1,19 @@
 package space.impact.game;
 
+import javax.swing.*;
+
 public class Play {
-    //
+    JFrame frame2;
+
+    public JFrame getFrame2() {
+        return frame2;
+    }
+
+    public void setFrame2(JFrame frame2) {
+        this.frame2 = frame2;
+    }
+
+    public Play(JFrame frame2) {
+        this.frame2 = frame2;
+    }
 }
--- src/space/impact/login/loginawal.java
+++ src/space/impact/login/loginawal.java
@@ -1,25 +1,43 @@
 package space.impact.login;
 
+import space.impact.game.Play;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputMethodListener;
 
-public class loginawal {
+public class loginawal{
     private JPanel menulogin;
-    private JTextField textField1;
+    private JTextField inputannama;
     private JButton mainButton;
+
+    private static JFrame frame = new JFrame("Login");
 
     public loginawal() {
         mainButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //
+                if (inputannama.getText().isEmpty()){
+                    JOptionPane.showMessageDialog(null,"Nama tidak boleh kosong!","Warning",JOptionPane.ERROR_MESSAGE);
+                }
+                else {
+                    if (mainButton.isEnabled()) {
+                        Play f = new Play(new JFrame("Tes"));
+                        f.getFrame2().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        f.getFrame2().pack();
+                        f.getFrame2().setVisible(true);
+                        f.getFrame2().setSize(1000, 600);
+                        f.getFrame2().setLocationRelativeTo(null);
+                        frame.setVisible(false);
+                    }
+                }
             }
         });
+
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Login");
         frame.setContentPane(new loginawal().menulogin);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
